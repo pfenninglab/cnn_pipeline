6438904f600345893955bafd786fcee3892596c9
Carson Sestili <sestilic@gmail.com>
2022-04-13 17:10:49 -0400

    WIP add modisco normalization

--- sketches/explain.py
+++ sketches/explain.py
@@ -14,10 +14,10 @@ import utils
 
 
 # TODO convert constants to config settings
-NUM_BG = 100
-NUM_FG = 20
+NUM_BG = 20
+NUM_FG = 5
 MODEL_PATH = "/home/csestili/repos/mouse_sst/wandb/run-20220408_112918-eg1rb9tq/files/model-best.h5"
-
+POS_LABEL = 1
 
 def explain():
     init()
@@ -42,16 +42,79 @@ def get_data(num_bg, num_fg):
 
 def get_deepshap_scores(model_path, bg, fg):
 	model = models.load_model(model_path)
-
 	explainer = shap.DeepExplainer(model, bg)
 	shap_values = explainer.shap_values(fg)
 
 	return shap_values
 
+class ModiscoNormalization:
+	VALID_NORMALIZATION_TYPES = ['none', 'gkm_explain', 'pointwise']
+
+	def __init__(self, normalization_type):
+		normalization_type = normalization_type.lower()
+		if normalization_type not in self.VALID_NORMALIZATION_TYPES:
+			raise ValueError(f"Invalid normalization type. Allowed values are {self.VALID_NORMALIZATION_TYPES}, got {normalization_type}")
+		self.normalization_type = normalization_type
+
+	def __call__(self, hyp_impscores, sequences):
+		"""
+		Args:
+			hyp_impscores [num_sequences, seq_len, 4]: hypothetical importance scores
+			sequences [num_sequences, seq_len, 4]: 1-hot encoded actual sequences
+
+		Returns:
+			normed_impscores [num_sequences, seq_len, 4]
+			normed_hyp_impscores [num_sequences, seq_len, 4]
+		"""
+		if self.normalization_type == 'none':
+			return self.identity_normalization(hyp_impscores, sequences)
+		elif self.normalization_type == 'gkm_explain':
+			return self.gkm_explain_normalization(hyp_impscores, sequences)
+		elif self.normalization_type == 'pointwise':
+			return self.pointwise_normalization(hyp_impscores, sequences)
+		else:
+			raise NotImplementedError(self.normalization_type)
+
+	@staticmethod
+	def identity_normalization(hyp_impscores, sequences):
+		normed_hyp_impscores = hyp_impscores
+		normed_impscores = normed_hyp_impscores * sequences
+		return normed_impscores, normed_hyp_impscores
+
+	@staticmethod
+	def gkm_explain_normalization(hyp_impscores, sequences):
+		# implements equations (27) and (28) from A.3 of GkmExplain supplementary material
+		# https://academic.oup.com/bioinformatics/article/35/14/i173/5529147#supplementary-data
+
+		# actual_scores is f_h(S_x, i, S_x^i)
+		actual_scores = np.sum(hyp_impscores * sequences, axis=-1)[:, np.newaxis]
+		numerator = hyp_impscores * actual_scores
+		denominator = np.sum(hyp_impscores * (hyp_impscores * actual_scores > 0), axis=-1)[:, np.newaxis]
+		normed_hyp_impscores = numerator / denominator
+		normed_impscores = normed_hyp_impscores * sequences
+		return normed_impscores, normed_hyp_impscores
+
+	@staticmethod
+	def pointwise_normalization(hyp_impscores, sequences):
+		# adapted from https://github.com/kundajelab/tfmodisco/blob/master/examples/H1ESC_Nanog_gkmsvm/TF%20MoDISco%20Nanog.ipynb
+		# TODO vectorize
+		normed_hyp_impscores = np.array([x - np.mean(x, axis=-1)[:,None] for x in hyp_impscores])
+		normed_impscores = normed_hyp_impscores * sequences
+		return normed_impscores, normed_hyp_impscores
+
+def _test_gkm_explain_normalization():
+	hyp_impscores = np.array([[1, -2, 3, -4], [-5, 6, -7, 8], [-9, 1, 2, -3]])
+	sequences     = np.array([[0,  1, 0,  0], [ 0, 0,  0, 1], [ 1, 0, 0,  0]])
+
+	numerator     = np.array([[-2, 4, -6, 8], [-40, 48, -56, 64], [81, -9, -18, 27]])
+	# TODO denominator sum parts
+
+	# TODO actually test
+
 def get_modisco_results(shap_values, fg):
-	# NOTE currently these scores are NOT normalized
-	normed_impscores = shap_values[0]
-	normed_hyp_impscores = shap_values[0]
+	hyp_imp_scores = shap_values[POS_LABEL]
+	normalization = ModiscoNormalization('pointwise')
+	normed_impscores, normed_hyp_impscores = normalization(hyp_imp_scores, fg)
 
 	seqlets_to_patterns_factory = modisco.tfmodisco_workflow.seqlets_to_patterns.TfModiscoSeqletsToPatternsFactory(
         trim_to_window_size=11,
