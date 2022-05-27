05bf1a893b756b0aca3a11629f3da50404320b67
Carson Sestili <sestilic@gmail.com>
2022-05-27 16:18:41 -0400

    add sst evaluations

---
+++ sst_evaluations/sst_evaluations.py
@@ -0,0 +1,56 @@
+# allow importing from one directory up
+import sys
+sys.path.append('..')
+import os.path
+
+import numpy as np
+import pandas as pd
+
+import dataset, models
+
+model_paths = [
+	("/home/csestili/repos/mouse_sst_clean/wandb/run-20220525_113317-pzz35myd/files/model-best.h5", "best auprc"),
+	("/home/csestili/repos/mouse_sst_clean/wandb/run-20220525_080410-dho6horg/files/model-best.h5", "best sensitivity with precision > 0.5")
+]
+
+eval_dir = "/projects/pfenninggroup/mouseCxStr/NeuronSubtypeATAC/Zoonomia_CNN/evaluations/SST/"
+eval_files = [
+	("Eval1_mm10_VAL.fa", 1),
+	("Eval2_hg38_VAL.fa", 0),
+	("Eval3_hg38_VAL.fa", 1),
+	("Eval4_mm10_VAL.fa", 0),
+	("Eval5_mm10_VAL.fa", 1),
+	("Eval6_mm10_VAL.fa", 1),
+	("Eval7_mm10_VAL.fa", 0),
+	("Eval8_mm10_VAL.fa", 1),
+	("Eval9_mm10_VAL.fa", 1),
+	("Eval10_mm10_VAL.fa", 0)
+]
+
+out_path = "/home/csestili/repos/mouse_sst/sst_evaluations/results.csv"
+
+def do_all_evals():
+	results = []
+	for model_path, desc in model_paths:
+		for idx, (eval_file, expected_label) in enumerate(eval_files):
+			eval_fa_path = os.path.join(eval_dir, eval_file)
+			res = validate(model_path, eval_fa_path, expected_label)
+
+			res['eval_num'] = idx + 1
+			res['model_path'] = model_path
+			res['desc'] = desc
+			results.append(res)
+
+	results = pd.DataFrame(results).round(4)
+	columns = ['desc', 'model_path', 'acc']
+	results.to_csv(out_path, index=False, columns=columns)
+
+def validate(model_path, eval_fa, expected_label):
+	model = models.load_model(model_path)
+	# TODO more elegant way to get correct labels
+	val_data = dataset.SequenceTfDataset([eval_fa], [expected_label], targets_are_classes=True, endless=False)
+	y = np.ones_like(val_data.dataset[1]) * expected_label
+	return model.evaluate(x=val_data.dataset[0], y=y, batch_size=512, return_dict=True)
+
+if __name__ == '__main__':
+	do_all_evals()
