56bcbb6480a2a3deaa77ede276b6bcdacaf84dd4
Carson Sestili <sestilic@gmail.com>
2022-06-28 12:59:18 -0400

    log optimizer attributes

--- callbacks.py
+++ callbacks.py
@@ -18,6 +18,7 @@ class LRLogger(tf.keras.callbacks.Callback):
     def __init__(self, optimizer):
         super(LRLogger, self).__init__()
         self.optimizer = optimizer
+        self.optimizer_attributes = self._get_optimizer_attributes()
 
     def on_epoch_begin(self, epoch, logs):
         if epoch == 0:
@@ -26,8 +27,15 @@ class LRLogger(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs):
         self._log()
 
+    def _get_optimizer_attributes(self):
+        """Get all numerical attributes of this optimizer."""
+        attrs = [k for k, v in self.optimizer.get_config().items() if isinstance(v, float)]
+        return attrs
+
     def _log(self):
-        wandb.log({"lr": self.optimizer.learning_rate(self.optimizer.iterations)}, commit=False)
+        data = {"lr": self.optimizer.learning_rate(self.optimizer.iterations)}
+        data.update({f"optim.{k}": getattr(self.optimizer, k) for k in self.optimizer_attributes})
+        wandb.log(data, commit=False)
 
 def get_early_stopping_callbacks(config):
     if config.get('early_stopping_callbacks') is None:
