294ce497d39d642d70cc4dce50581cdf9ca98b93
Carson Sestili <sestilic@gmail.com>
2022-07-07 16:03:23 -0400

    use env variables in scripts

---
+++ train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Usage: bash train.sh <path to config>
+
+# Check environment variables
+if [[ -z $PARTITION_GPU ]]
+then
+    echo "Error: \$PARTITION_GPU is not set."
+    exit 1
+fi
+
+# Check arguments
+config_path=$1
+
+if [ -z $config_path ];
+then
+    echo "Error: Missing arguments"
+    echo "Usage: bash train.sh <path to config>"
+    exit 1
+fi
+
+sbatch -p $PARTITION_GPU scripts/train_main.sb $config_path
