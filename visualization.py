213e1c3b2545b95d2a08582dc280cf6f20e1eae5
Carson Sestili <sestilic@gmail.com>
2022-09-20 14:13:06 -0400

    color points in UMAP visualization

--- visualization.py
+++ visualization.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import umap
 
-def get_umap(fit_data, transform_data=None, transform_outfile=None, plot_outfile=None, umap_kwargs=None, scatter_kwargs=None):
+def get_umap(fit_data, fit_labels=None, transform_data=None, transform_labels=None, transform_outfile=None, plot_outfile=None, umap_kwargs=None, scatter_kwargs=None):
 	"""Fit a UMAP reducer, and optionally transform and plot.
 
 	Args:
-		fit_data (np array): Data to fit the UMAP transform.
-		transform_data (np array): Data to transform after UMAP is fit.
-			if not passed, then fit_data will be transformed.
+		fit_data (np array), shape (num_samples, num_features): Data to fit the UMAP transform.
+		fit_labels (np array), shape (num_samples,): If given, then use labels for
+			supervised dimension reduction.
+		transform_data (np array), shape (num_samples_tx, num_features): Data to transform
+			after UMAP is fit. If not passed, then fit_data will be transformed.
+		transform_labels (np array), shape (num_samples_tx,): If given, then use these labels
+			to color the points in the visualization.
 		transform_outfile (str): Path to save transformed data, .npy.
 		plot_outfile (str): Path to save plot of transformed data, .jpg or .png.
 		scatter_kwargs (dict): Keyword arguments to matplotlib scatter().
@@ -18,19 +22,31 @@ def get_umap(fit_data, transform_data=None, transform_outfile=None, plot_outfile
 	# Fit UMAP transform
 	umap_kwargs = umap_kwargs or {}
 	reducer = umap.UMAP(**umap_kwargs)
-	reducer.fit(fit_data)
+	print("Fitting UMAP transform...")
+	reducer.fit(fit_data, y=fit_labels)
 
 	# Transform data
 	if transform_data is None:
 		transform_data = fit_data
+	print("Transforming data...")
 	transformed = reducer.transform(transform_data)
 	if transform_outfile is not None:
 		np.save(transform_outfile, transformed)
 
 	# Plot transformed data
 	if plot_outfile is not None:
+		print("Plotting...")
+		# Create default scatter_kwargs as an empty dict
 		scatter_kwargs = scatter_kwargs or {}
-		plt.scatter(transform[:, 0], transform[:, 1], **scatter_kwargs)
+		if transform_labels is not None:
+			# Color the samples by their label
+			scatter_kwargs['c'] = transform_labels
+			scatter_kwargs['cmap'] = 'Spectral'
+		plt.scatter(transformed[:, 0], transformed[:, 1], **scatter_kwargs)
+		if transform_labels is not None:
+			# Add color legend
+			num_labels = len(set(transform_labels))
+			plt.colorbar(boundaries=np.arange(num_labels + 1) - 0.5).set_ticks(np.arange(num_labels))			
 		plt.savefig(plot_outfile)
 
-	return reducer, transformed_data
+	return reducer, transformed
