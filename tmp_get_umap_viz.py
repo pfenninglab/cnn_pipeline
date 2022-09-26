8cf91a30e9481a803a96e22989e101f9f35c41e8
Carson Sestili <sestilic@gmail.com>
2022-09-26 14:33:25 -0400

    WIP UMAP single fit, multiple transforms

---
+++ tmp_get_umap_viz.py
@@ -0,0 +1,93 @@
+import os
+import pickle
+
+import numpy as np
+from tqdm import tqdm
+
+import visualization
+import models
+
+
+# multispecies PV model
+MODEL_PATH = '/projects/pfenninggroup/mouseCxStr/NeuronSubtypeATAC/Zoonomia_CNN/multispecies_PV/models/FINAL_modelmultiPVi.h5'
+LAYER_NAME = 'activation_1'
+# mouse-only PV model
+# MODEL_PATH = '/projects/pfenninggroup/mouseCxStr/NeuronSubtypeATAC/Zoonomia_CNN/mouse_PV/models/FINAL_modelPV3e.h5'
+# [...] -> maxpool -> flatten -> dense(300) -> activation_9 -> [...]
+#LAYER_NAME = 'activation_9'
+
+
+
+DATA_DIR = '/projects/pfenninggroup/mouseCxStr/NeuronSubtypeATAC/Zoonomia_CNN/multispecies_PV/FinalModelData'
+FG_POS_PATH = os.path.join(DATA_DIR, 'mouse_PV_pos_VAL.fa')
+FG_NEG_PATH_FIT = os.path.join(DATA_DIR, 'mouse_PV_neg_VAL.fa')
+# mouse non-enhancer orthologs of human enhancers
+FG_NEG_PATH_TX = '/projects/pfenninggroup/mouseCxStr/NeuronSubtypeATAC/Zoonomia_CNN/evaluations/PV/Eval4_mm10.fa'
+BG_POS_PATH = os.path.join(DATA_DIR, 'human_PV_pos_VAL.fa')
+BG_NEG_PATH_FIT = os.path.join(DATA_DIR, 'human_PV_neg_VAL.fa')
+# human non-enhancer orthologs of mouse enhancers
+BG_NEG_PATH_TX = '/projects/pfenninggroup/mouseCxStr/NeuronSubtypeATAC/Zoonomia_CNN/evaluations/PV/Eval2_hg38.fa'
+ACTIVATIONS_DIR = '/home/csestili/data/tacit_viz/mouse_pv/'
+
+def main():
+	# Get activations
+	print("Getting activations...")
+	activations = {}
+	if not os.path.exists(ACTIVATIONS_DIR):
+		os.makedirs(ACTIVATIONS_DIR)
+	model = None
+	for (path, name) in tqdm([
+		(FG_POS_PATH, 'fg_pos'), (FG_NEG_PATH_FIT, 'fg_neg_fit'), (FG_NEG_PATH_TX, 'fg_neg_tx'),
+		(BG_POS_PATH, 'bg_pos'), (BG_NEG_PATH_FIT, 'bg_neg_fit'), (BG_NEG_PATH_TX, 'bg_neg_tx')]):
+		activations_path = os.path.join(ACTIVATIONS_DIR, f"{name}.npy")
+		if not os.path.exists(activations_path):
+			if model is None:
+				model = models.load_model(MODEL_PATH)
+			activations[name] = models.get_activations(
+				model, path, out_file=activations_path, layer_name=LAYER_NAME)
+		else:
+			activations[name] = np.load(activations_path)
+
+	# Fit UMAP on the val data seen during training
+	reducer_path = os.path.join(ACTIVATIONS_DIR, f"umap_reducer.pkl")
+	if not os.path.exists(reducer_path):
+		fit_data = np.concatenate((
+			activations['fg_pos'], activations['fg_neg_fit'],
+			activations['bg_pos'], activations['bg_neg_fit']), axis=0)
+		reducer = visualization.umap_fit(fit_data, reducer_outfile=reducer_path)
+	else:
+		with open(reducer_path, 'rb') as f:
+			reducer = pickle.load(f)
+
+	# Transform and visualize
+	# labels for all plots
+	for (set_a, label_a, set_b, label_b, name) in [
+		('fg_pos', 0, 'bg_pos', 2, 'umap_positives'),
+		('fg_pos', 0, 'fg_neg_tx', 1, 'umap_fg'),
+		('bg_pos', 2, 'bg_neg_tx', 3, 'umap_bg')]:
+
+		# Get the relevant activations
+		transform_data = np.concatenate((activations[set_a], activations[set_b]), axis=0)
+		transform_labels = np.array(
+			[label_a] * len(activations[set_a]) +
+			[label_b] * len(activations[set_b]))
+		# Add 1 instance from each class for legend colors
+		extra_points = np.stack(tuple(activations[set_name][0] for set_name in ['fg_pos', 'fg_neg_tx', 'bg_pos', 'bg_neg_tx']))
+		extra_labels = np.array([0, 1, 2, 3])
+		transform_data = np.concatenate((transform_data, extra_points), axis=0)
+		transform_labels = np.concatenate((transform_labels, extra_labels), axis=0)
+
+		# Shuffle for visibility of all classes
+		rng = np.random.default_rng()
+		combined = np.concatenate((transform_data, np.expand_dims(transform_labels, axis=1)), axis=1)
+		rng.shuffle(combined)
+		transform_data, transform_labels = combined[:, :-1], combined[:, -1]
+
+		# Transform and visualize
+		plot_outfile = os.path.join(ACTIVATIONS_DIR, f"{name}.png")
+		label_mapping = {0: 'mouse PV +', 1: 'mouse PV - (neoe)', 2: 'human PV +', 3: 'human PV - (neoe)'}
+		visualization.umap_transform(reducer, transform_data, transform_labels=transform_labels,
+			label_mapping=label_mapping, plot_outfile=plot_outfile, scatter_kwargs={"s": 1.5})
+
+if __name__ == '__main__':
+	main()
--- visualization.py
+++ visualization.py
@@ -1,4 +1,6 @@
 """visualization.py: Tools for visualizing data."""
+import pickle
+
 import matplotlib.pyplot as plt
 import numpy as np
 import umap
@@ -19,6 +21,8 @@ def get_umap(fit_data, fit_labels=None, transform_data=None, transform_labels=No
 		scatter_kwargs (dict): Keyword arguments to matplotlib scatter().
 		umap_kwargs (dict): Keyword arguments to create the UMAP reducer.
 	"""
+	# TODO DRY
+
 	# Fit UMAP transform
 	umap_kwargs = umap_kwargs or {}
 	reducer = umap.UMAP(**umap_kwargs)
@@ -41,12 +45,57 @@ def get_umap(fit_data, fit_labels=None, transform_data=None, transform_labels=No
 		if transform_labels is not None:
 			# Color the samples by their label
 			scatter_kwargs['c'] = transform_labels
-			scatter_kwargs['cmap'] = 'Spectral'
+			scatter_kwargs['cmap'] = 'gist_rainbow'
 		plt.scatter(transformed[:, 0], transformed[:, 1], **scatter_kwargs)
 		if transform_labels is not None:
 			# Add color legend
 			num_labels = len(set(transform_labels))
 			plt.colorbar(boundaries=np.arange(num_labels + 1) - 0.5).set_ticks(np.arange(num_labels))			
-		plt.savefig(plot_outfile)
+		plt.savefig(plot_outfile, dpi=300)
 
 	return reducer, transformed
+
+def umap_fit(fit_data, fit_labels=None, umap_kwargs=None, reducer_outfile=None):
+	# Fit UMAP transform
+	umap_kwargs = umap_kwargs or {}
+	reducer = umap.UMAP(**umap_kwargs)
+	print("Fitting UMAP transform...")
+	reducer.fit(fit_data, y=fit_labels)
+
+	# Save fit reducer object
+	if reducer_outfile is not None:
+		with open(reducer_outfile, 'wb') as f:
+			pickle.dump(reducer, f)
+
+	return reducer
+
+def umap_transform(reducer, transform_data, transform_labels=None, label_mapping=None, transform_outfile=None, plot_outfile=None, scatter_kwargs=None):
+	# Transform data
+	print("Transforming data...")
+	transformed = reducer.transform(transform_data)
+	if transform_outfile is not None:
+		np.save(transform_outfile, transformed)
+
+	# Plot transformed data
+	if plot_outfile is not None:
+		print("Plotting...")
+		# Create default scatter_kwargs as an empty dict
+		scatter_kwargs = scatter_kwargs or {}
+		if transform_labels is not None:
+			# Color the samples by their label
+			scatter_kwargs['c'] = transform_labels
+			scatter_kwargs['cmap'] = 'cool'
+		plt.clf()
+		plot = plt.scatter(transformed[:, 0], transformed[:, 1], **scatter_kwargs)
+		if transform_labels is not None:
+			# Convert numerical labels to string in the legend
+			lines, labels = plot.legend_elements()
+			if label_mapping is not None:
+				# Assumes labels is sorted unique transform labels
+				labels = [label_mapping[itm] for itm in sorted(set(transform_labels))]
+			# Add color legend and format figure
+			plt.legend(lines, labels, loc='lower right', prop={'size': 5})
+			plt.tick_params(labelbottom=False, bottom=False, labelleft=False, left=False)
+		plt.savefig(plot_outfile, dpi=300)
+
+	return transformed
