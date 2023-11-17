a89a15803d8ad95af672e6de1695e638a4d75203
Heather Harper Sestili <sestilic@gmail.com>
2023-11-17 13:49:40 -0500

    docstring update

--- scripts/get_activations.py
+++ scripts/get_activations.py
@@ -2,22 +2,30 @@
 
 Usage: python scripts/get_activations.py \
 	-model <path to model .h5>
-	-in_file <path to input .fa, .bed, or .narrowPeak file> \
-	[-in_genome <path to genome .fa file, if in_file is .bed or .narrowPeak>] \
+	-in_files <paths to input .fa, .bed, or .narrowPeak file> \
+	[-in_genomes <paths to genome .fa file, if in_file is .bed or .narrowPeak>] \
 	-out_file <path to output file, .npy or .csv> \
 	[-layer_name <layer name to get activations from, e.g. 'flatten'>. default is output layer] \
 	[--no_reverse_complement, don't evaluate on reverse complement sequences] \
 	[--write_csv, write activations as .csv file instead of .npy] \
 	[-score_column <output unit to extract score in the csv, e.g. 1>. default writes whole activation as a row]
 
-To get a numpy array of activations from an intermediate layer:
-	-layer_name <layer_name>
-	[don't pass --write_csv]
+Examples:
 
-To get a csv of probabilities for the positive class from a binary classifier:
+1. Model is a binary classifier, output .csv file of probabilities for the positive class:
 	[don't pass -layer_name]
 	--write_csv
 	-score_column 1
+
+2. Model is a regression model, output .csv file of predicted values:
+	[don't pass -layer_name]
+	--write_csv
+	-score_column 0
+
+3. Model is classification or regression, output .npy file of inner-layer activations:
+	-layer_name <layer_name>
+	[don't pass --write_csv]
+
 """
 import argparse
 
